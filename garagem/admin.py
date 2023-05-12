3b82c7a1ffd20e6ddf0fe994713537955ec48b23
thesh1ro <gabrielmonteiro1111@gmail.com>
2023-05-12 12:29:03 -0300

    Criação de modelos e admin

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from . models import Categoria, Veiculo, Acessorio, Cor, Marca, Modelo
+
 # Register your models here.
+admin.site.register(Marca)
+admin.site.register(Categoria)
+admin.site.register(Cor)
+admin.site.register(Acessorio)
+admin.site.register(Veiculo)
+admin.site.register(Modelo)
